39690e2714df401fe31fdb172a72f4a1d3253ce2
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2024-02-01 19:20:35 -0600

    Stepping Stones

---
+++ Aidens-NEO-Swerve-Imported/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,124 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final class ControllerConstants{
+    // has our joystick and controller usb ports
+    public static final int kLeftJoystickControllerPort = 0;
+    public static final int kRightJoystickControllerPort = 1;
+    public static final int kXboxControllerPort = 3;
+  }
+  public static final class ModuleConstants {
+    //gear ratios wheel diameters and encoder numbers
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final double kDriveMotorGearRatio = 1/ 6.75; // look up the module gear ratio (confirmed)
+    public static final double kTurningMotorGearRatio = 1 / 12.8; // confirmed for all mk4 modules
+    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+    public static final double kPTurning = 0.5;
+  }
+
+  public static final class SensorConstants{
+    // extra sensors
+    public static final int kPigeonID = 13;
+  }
+
+  public static final class OIConstants{
+    //make this bigger  if it moves on its own lol
+    public static final double kDeadband = 0.2;
+
+  }
+  public static final class DriveConstants{
+    // configure our robot dimensions and motor controller/encoder ports
+    public static final double kTrackWidth = Units.inchesToMeters(34.5);
+    //distance between right and left wheels
+    public static final double kWheelBase = Units.inchesToMeters(34.5);
+    //distance between front and back wheels
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, -kTrackWidth/ 2),
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2)
+    );
+
+    //motor and encoder ports IMPORTANT
+
+    //Front Left
+    public static final int kFrontLeftDriveMotorPort = 55;
+    public static final int kFrontLeftTurningMotorPort = 54; // fill these out then delete this comment
+    public static final boolean kFrontLeftDriveEncoderReversed = false;
+    public static final boolean kFrontLeftTurningEncoderReversed = true;
+    public static final int kFrontLeftDriveAbsoluteEncoderPort = 61;
+    public static final double kFrontLeftDriveAbsoluteEncoderOffsetAng = 268;
+    public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = true;
+
+    //Front Right
+    public static final int kFrontRightDriveMotorPort = 53;
+    public static final int kFrontRightTurningMotorPort = 51;
+    public static final boolean kFrontRightDriveEncoderReversed = true;
+    public static final boolean kFrontRightTurningEncoderReversed = true;
+    public static final int kFrontRightDriveAbsoluteEncoderPort = 62;
+    public static final double kFrontRightDriveAbsoluteEncoderOffsetAng = 160;
+    public static final boolean kFrontRightDriveAbsoluteEncoderReversed = true;
+ 
+    //Back Left
+    public static final int kBackLeftDriveMotorPort = 43;
+    public static final int kBackLeftTurningMotorPort = 50;
+    public static final boolean kBackLeftDriveEncoderReversed = true;
+    public static final boolean kBackLeftTurningEncoderReversed = true;
+    public static final int kBackLeftDriveAbsoluteEncoderPort = 60;
+    public static final double kBackLeftDriveAbsoluteEncoderOffsetAng = 10;
+    public static final boolean kBackLeftDriveAbsoluteEncoderReversed = true;
+
+    //Back Right
+    public static final int kBackRightDriveMotorPort = 56;
+    public static final int kBackRightTurningMotorPort = 57;
+    public static final boolean kBackRightDriveEncoderReversed = false;
+    public static final boolean kBackRightTurningEncoderReversed = true;
+    public static final int kBackRightDriveAbsoluteEncoderPort = 59;
+    public static final double kBackRightDriveAbsoluteEncoderOffsetAng = 308;
+    public static final boolean kBackRightDriveAbsoluteEncoderReversed = true;
+
+    // run at full speed to test these then delete this comment
+    public static final double kPhysicalMaxSpeedMetersPerSecond = 4;
+    public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 7 * 2 * Math.PI;
+
+    public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond / 1.2;
+        public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = //
+                kPhysicalMaxAngularSpeedRadiansPerSecond / 10;
+        public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 8;
+        public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = Math.PI * 2;
+  }
+
+  public static final class AutoConstants{
+    public static final double kMaxSpeedMetersPerSecond = 1;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 0.1;
+    public static final double kPXController = 1.5;
+    public static final double kPYController = 1.5;
+    public static final double kPThetaController = 1.5;
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+        new TrapezoidProfile.Constraints(
+          kMaxSpeedMetersPerSecond, 
+        kMaxAccelerationMetersPerSecondSquared);
+    public static double kThetaController;
+  }
+}
---
+++ Aidens-NEO-Swerve-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,140 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.SwerveJoystickCmd;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.ShuffleboardInfo;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final String placementone = "Robot 1", placementtwo = "Robot 2", placementthree = "Robot 3";
+  
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  private final ShuffleboardInfo shuffleboardinfo = new ShuffleboardInfo(swerveSubsystem);
+  public static XboxController Xboxcontroller = new XboxController(ControllerConstants.kXboxControllerPort);
+  public RobotContainer() {
+    swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
+      swerveSubsystem,
+      () -> Xboxcontroller.getLeftY(),
+      () -> -Xboxcontroller.getLeftX(),
+      () -> -Xboxcontroller.getRightX(),
+      () -> Xboxcontroller.getRightBumper()));
+
+    
+    configureBindings();
+
+    m_chooser.addOption(placementone, placementone);
+    m_chooser.addOption(placementtwo, placementtwo);
+    m_chooser.addOption(placementthree, placementthree);
+
+    
+    ShuffleboardTab driverBoard = Shuffleboard.getTab("Driver Board");
+    driverBoard.add("Auto choices", m_chooser).withWidget(BuiltInWidgets.kComboBoxChooser);
+  }
+
+  private void configureBindings() {
+    new JoystickButton(Xboxcontroller, 2).onTrue(() -> swerveSubsystem.zeroHeading());
+  }
+
+  public Command getAutonomousCommand() {
+    System.out.println("Autos Begun");
+    // create auto path settings
+    // later move this part into a file of all our possible autos and make this the choosing section
+    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, 
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        .setKinematics(DriveConstants.kDriveKinematics);
+    
+    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+      new Pose2d(0,0, new Rotation2d(0)),
+      List.of( 
+        new Translation2d(1,0), 
+        new Translation2d(1, -1)
+      ),
+      new Pose2d(2,-1, Rotation2d.fromDegrees(180)),
+      trajectoryConfig
+    );
+
+    PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+    PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+    ProfiledPIDController thetaController = new ProfiledPIDController(
+      AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints
+      );
+      thetaController.enableContinuousInput(-Math.PI, Math.PI);
+       
+      m_autoSelected = m_chooser.getSelected();
+
+      if (m_autoSelected == placementone)
+      return new ParallelCommandGroup(CommandSequences.robot1Command(swerveSubsystem));
+
+      if (m_autoSelected == placementtwo)
+      return new ParallelCommandGroup(null);
+
+      if (m_autoSelected == placementthree)
+      return new ParallelCommandGroup(null);
+
+      
+      
+      SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+        trajectory,
+        swerveSubsystem::getPose,
+        DriveConstants.kDriveKinematics,
+        xController,
+        yController,
+        thetaController,
+        swerveSubsystem::setModuleStates,
+        swerveSubsystem 
+      );
+      return new SequentialCommandGroup(
+          new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
+          swerveControllerCommand, //starts the movement
+          new InstantCommand(() -> swerveSubsystem.stopModules()) // stops the motors when
+      );
+  }
+  public void logSwerve(){
+ 
+    SmartDashboard.putNumber("Heading", swerveSubsystem.getHeading());
+    SmartDashboard.putString("Robot Location", swerveSubsystem.getPose().getTranslation().toString());
+    SmartDashboard.putNumber("frontLeft Encoder", swerveSubsystem.getFLAbsEncoder());
+    SmartDashboard.putNumber("frontRight Encoder", swerveSubsystem.getFRAbsEncoder());
+    SmartDashboard.putNumber("BackLeft Encoder", swerveSubsystem.getBLAbsEncoder());
+    SmartDashboard.putNumber("BackRight Encoder", swerveSubsystem.getBRAbsEncoder());
+  }
+}
